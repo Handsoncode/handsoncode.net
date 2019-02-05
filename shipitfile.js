016f795e8af666b545d5f35eb2fd6bec520e58a4
budnix <571316+budnix@users.noreply.github.com>
2019-02-05 09:40:08 +0100

    Change repository url

--- shipitfile.js
+++ shipitfile.js
@@ -6,7 +6,7 @@ module.exports = (shipit) => {
       servers: 'deploy@142.4.202.189:22022',
       workspace: '/tmp/handsoncode.net',
       deployTo: '/home/httpd/handsoncode.net',
-      repositoryUrl: 'https://github.com/handsoncode/handsoncode.net.git',
+      repositoryUrl: 'git@github.com:handsoncode/handsoncode.net.git',
       branch: 'master',
       ignores: ['.git', 'node_modules', 'shipitfile.js'],
       rsync: ['--force', '--delete', '--delete-excluded', '-I', '--stats', '--chmod=ug=rwX'],
